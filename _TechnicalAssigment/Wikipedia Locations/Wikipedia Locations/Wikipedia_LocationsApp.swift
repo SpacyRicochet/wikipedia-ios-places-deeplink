78e57b6cd0b75cf181e3690853ecf7d462e4b4e0
Bruno Scheele <bruno@noodlewerk.com>
2023-12-06 14:54:49 +0100

    add: skeleton project for Wikipedia Locations

---
+++ _TechnicalAssigment/Wikipedia Locations/Wikipedia Locations/Wikipedia_LocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wikipedia_LocationsApp.swift
+//  Wikipedia Locations
+//
+//  Created by Bruno Scheele on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Wikipedia_LocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
